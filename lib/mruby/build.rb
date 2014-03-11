02db9f14e7c0cf46d2142a8864b7dbef274254eb
Akira Yumiyama <yumiyama.akira@gmail.com>
2014-03-11 22:23:37 +0900

    init mruby-build.gem

---
+++ lib/mruby/build.rb
@@ -0,0 +1,7 @@
+require "mruby/build/version"
+
+module Mruby
+  module Build
+    # Your code goes here...
+  end
+end
---
+++ lib/mruby/build/version.rb
@@ -0,0 +1,5 @@
+module Mruby
+  module Build
+    VERSION = "0.0.1"
+  end
+end
