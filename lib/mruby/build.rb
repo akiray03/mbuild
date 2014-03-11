eb305f4dd41127143039d9202cc52e6ca4ca0301
Akira Yumiyama <yumiyama.akira@gmail.com>
2014-03-12 08:25:20 +0900

    initialize code

--- lib/mruby/build.rb
+++ lib/mruby/build.rb
@@ -1,7 +1,17 @@
+require 'fileutils'
+require 'optparse'
+require 'term/ansicolor'
+
 require "mruby/build/version"
+
+class String
+  include Term::ANSIColor
+end
 
 module Mruby
   module Build
-    # Your code goes here...
+    def self.run argv
+      p argv
+    end
   end
 end
